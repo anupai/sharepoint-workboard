aa3f59afaa611d8a7e18730f19095b6d52885f97
unknown <anupai92@gmail.com>
2018-06-19 15:56:16 +1000

    sharepoint workbench-setup

---
+++ src/webparts/workBoard/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IWorkBoardWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'WorkBoardWebPartStrings' {
+  const strings: IWorkBoardWebPartStrings;
+  export = strings;
+}
